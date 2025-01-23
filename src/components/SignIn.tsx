6e74c13428ef9a18db8872fcc2068b19ca34565b
Aveskulov <WenJoymk3@gmail.com>
2025-01-23 13:07:11 +0100

    signIn logic with firebase

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,30 +1,64 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
+import { auth } from '../firebase/firebase-config';
 
 const SignIn: React.FC = () => {
-	const [email, setEmail] = useState<string>();
-	const [password, setPassword] = useState<string>();
-	const [passwordCopy, setPasswordCopy] = useState<string>();
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+	const [passwordCopy, setPasswordCopy] = useState<string>('');
+	const [error, setError] = useState<string | null>(null);
 	const handlerSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		console.log(event);
+		if (passwordCopy !== password) {
+			setError("passwords didn't match");
+			return 0;
+		}
+		createUserWithEmailAndPassword(auth, email, password)
+			.then(user => {
+				console.log(user);
+				setEmail('');
+				setPassword('');
+				setPasswordCopy('');
+			})
+			.catch(error => console.log(error));
 	};
+
 	return (
 		<div className='register'>
 			<h2>Register</h2>
 			<form onSubmit={handlerSubmit}>
 				<label htmlFor='signEmail'>
 					Email:
-					<input type='email' id='signEmail' value={email} />
+					<input
+						placeholder='Enter email address'
+						onChange={e => setEmail(e.target.value)}
+						type='email'
+						id='signEmail'
+						value={email}
+					/>
 				</label>
 				<label htmlFor='signPassword'>
 					Password:
-					<input type='password' id='signPassword' value={password} />
+					<input
+						placeholder='Enter password'
+						onChange={e => setPassword(e.target.value)}
+						type='password'
+						id='signPassword'
+						value={password}
+					/>
 				</label>
 				<label htmlFor='signPasswordCopy'>
 					Repeat your password:
-					<input type='password' id='signPasswordCopy' value={passwordCopy} />
+					<input
+						placeholder='Repeat password'
+						onChange={e => setPasswordCopy(e.target.value)}
+						type='password'
+						id='signPasswordCopy'
+						value={passwordCopy}
+					/>
 				</label>
 				<button>Create</button>
+				{error ? <p>{error}</p> : ''}
 			</form>
 		</div>
 	);
--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -1,12 +1,15 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APPI_KEY,
-	authDomain: process.env.REACT_AUTH_DOMAIN,
-	projectId: process.env.REACT_PROJECT_ID,
-	storageBucket: process.env.REACT_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_MESSAGING,
-	appId: process.env.REACT_APP_ID,
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
